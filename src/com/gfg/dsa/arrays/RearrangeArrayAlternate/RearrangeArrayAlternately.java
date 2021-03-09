e0034f0cdfb667220f47a8eec20443485bd306b1
Rupesh <rupeshkumar9312@gmail.com>
2021-03-09 14:48:42 -0800

    Rearrange Array Alternately

---
+++ src/com/gfg/dsa/arrays/RearrangeArrayAlternate/RearrangeArrayAlternately.java
@@ -0,0 +1,77 @@
+package com.gfg.dsa.arrays.RearrangeArrayAlternate;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+// 		Scanner in = new Scanner(System.in);
+
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        //testcases
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            //size of array
+            int n = Integer.parseInt(read.readLine());
+            int[] temp = new int[n];
+
+            String str[] = read.readLine().trim().split(" ");
+
+            //adding elements to the array
+            for (int i = 0; i < n; i++)
+                temp[i] = Integer.parseInt(str[i]);
+
+            // StringBuffer sb = new StringBuffer();
+
+            Solution ob = new Solution();
+
+            //calling rearrange() function
+            ob.rearrange(temp, n);
+            StringBuffer sb = new StringBuffer();
+
+            //appending and printing the elements
+            for (int i = 0; i < n; i++)
+                sb.append(temp[i] + " ");
+            System.out.println(sb);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(int arr[], int n) {
+
+        int[] newArr = new int[n];
+        int dec = 0;
+        for (int i = 0; i < n; i++) {
+            int index = n - dec - 1;
+            newArr[i] = arr[index];
+            i++;
+            if (i < n) {
+                newArr[i] = arr[dec];
+            }
+            dec++;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = newArr[i];
+        }
+        // Your code here
+
+    }
+
+}
+
+
+
